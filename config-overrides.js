39b85390bc9fa170f29557a4391c0c26062b0c74
Andrey Synyuk <andrey.sinyuk@triogeeks.com>
2025-02-05 18:57:59 +0200

    commit #4

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
-
-module.exports = function override(config) {
-    if (config.mode === "production") {
-        config.optimization.minimizer.push(new HtmlMinimizerPlugin());
-    }
-    return config;
-};
